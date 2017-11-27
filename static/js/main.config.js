92e7b28b1900e6ae48b7a10e709dd25749ef6416
python-rocks <34040724+python-rocks@users.noreply.github.com>
2017-11-28 01:21:14 +0530

    Creating index.html and adding static files

---
+++ static/js/main.config.js
@@ -0,0 +1,18 @@
+var config = {
+    countdown: {
+        year: 2015,
+        month: 12,
+        day: 21,
+        hour: 12,
+        minute: 12,
+        second: 21
+    },
+
+    subscription_form_tooltips: {
+        success: 'Taking you there!',
+        already_subscribed: 'You are already subscribed',
+        empty_email: 'Please, Enter your url',
+        invalid_email: 'Url is invalid. Enter a valid url address',
+        default_error: 'Error! Contact administration'
+    }
+}
